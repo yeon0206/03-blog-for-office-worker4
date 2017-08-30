2132b14c9abd2469c521caabf0f79629c17e5e54
Giyeon <giyeon@Giyeonui-MacBook-Air.local>
2017-08-30 22:53:49 +0900

    admin,model

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Article,Comment,HashTag
+
+
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display=['id','title','posted_at','public']
+    list_display_links=['title']
+    list_editable=['public']
+
+admin.site.register(Article,ArticleAdmin)
 
 # Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=['id','__str__','username','posted_at','public']
+    list_display_links=['__str__']
+    list_editale=['public']
+
+admin.site.register(Comment,CommentAdmin)
+
+class HashTagAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(HashTag,HashTagAdmin)
--- feed/models.py
+++ feed/models.py
@@ -34,7 +34,10 @@ class Comment(AddTime):
     public = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.username
+        return "{}   Re:  {}".format(self.article.title, self.content)
 
 class HashTag(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
