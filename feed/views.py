08c784b0976089ddc62ff09fd3110f9bf03b0c66
Giyeon <giyeon@Giyeonui-MacBook-Air.local>
2017-08-31 16:01:32 +0900

    category error

--- feed/views.py
+++ feed/views.py
@@ -3,18 +3,44 @@ from .models import Article,Comment,HashTag
 # Create your views here.
 
 def index(request):
-    article_list = Article.objects.all()
+    #GET and posted_at
+    category = request.GET.get("category")
+
+    if not category: #카테고리가 존재하지 않으면
+        article_list = Article.objects.all()
+    else:
+        article_list = Article.objects.filter(category=category)
+
     hashtag_list = HashTag.objects.all()
+    # category_list = set([article.get_category_display() for article in article_list])
+
+    category_list = set([
+        (article.category, article.get_category_display())
+        for article in article_list
+        ])
+
+    # for article in article_list:
+    #     category_list.add(article.get_category_display())
+
+
     ctx={
-    "article_list" : article_list,
-    "hashtag_list" : hashtag_list,
+        "article_list" : article_list,
+        "hashtag_list" : hashtag_list,
+        "category_list" : category_list,
     }
+
+    # print(ctx.keys())
+    # print(ctx.values())
+    # print(ctx.items())
+
     return render(request, "index.html", ctx )
 
 def detail(request, article_id):
     article = Article.objects.get(id=article_id)
+    hashtag_list = HashTag.objects.all()
     ctx={
         "article" : article,
+        "hashtag_list" : hashtag_list,
     }
     return render(request, "detail.html", ctx)
 
