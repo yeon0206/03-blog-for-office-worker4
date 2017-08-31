2a2970b6f893fba85ac5018a28d84975a1ab0743
Giyeon <giyeon@Giyeonui-MacBook-Air.local>
2017-08-31 21:07:53 +0900

    category

--- feed/views.py
+++ feed/views.py
@@ -6,18 +6,16 @@ def index(request):
     #GET and posted_at
     category = request.GET.get("category")
 
-    if not category: #카테고리가 존재하지 않으면
-        article_list = Article.objects.all()
+    if not category:
+        article_list= Article.objects.all()
     else:
-        article_list = Article.objects.filter(category=category)
-
+        article_list= Article.objects.filter(category=category)
     hashtag_list = HashTag.objects.all()
-    # category_list = set([article.get_category_display() for article in article_list])
 
     category_list = set([
         (article.category, article.get_category_display())
         for article in article_list
-        ])
+    ])
 
     # for article in article_list:
     #     category_list.add(article.get_category_display())
